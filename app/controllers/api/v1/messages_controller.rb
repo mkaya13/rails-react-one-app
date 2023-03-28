2ee4e1105442fb6a1a263e941336a38aff676098
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-28 15:02:26 +0300

    Fixes linter errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,12 @@
-class Api::V1::MessagesController < ApplicationController
-    # Add sample function to return a random single sample.
-    def index
-      render json: Message.all.sample
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class MessagesController < ApplicationController
+      # Add sample function to return a random single sample.
+      def index
+        render json: Message.all.sample
+      end
     end
   end
+end
